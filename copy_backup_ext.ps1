5c70e252100ceea4609dbb0fa6c164394ee4fc85
Michael Stepniewski <mike@corp.flashtrucking.com>
2020-11-13 13:59:05 -0600

    new file:   copy_backup_ext.ps1 	new file:   system_image_backup.ps1

---
+++ copy_backup_ext.ps1
@@ -0,0 +1,14 @@
+﻿################################################
+#
+#  Create backup to external drive
+#
+#
+#  Author: Michael Stepniewski
+#  Contact: infotechpro.stepniewski@gmail.com
+#
+#################################################
+Write-Output "Backup to external storage starting...`n"
+
+robocopy G:\ E:\ /MIR /MT /V /ZB
+
+Write-Output "Backup to external storage Complete`n" + $(Get-Date -Format MM-dd-yyyy-HH-mm-ss)
---
+++ system_image_backup.ps1
@@ -0,0 +1,27 @@
+﻿################################################
+#
+#  Create a System Image Backup
+#  Backup to internal backup SSD
+#
+#  Author: Michael Stepniewski
+#  Contact: infotechpro.stepniewski@gmail.com
+#
+#################################################
+
+$backup_source = 'C:,D:'
+$backup_destination = 'G:'
+$logfile = 'C:\Log\BackupLog-' + $(Get-Date -Format MM-dd-yyyy-HH-mm-ss) + '.txt'
+
+#Create New Backup Log File
+New-Item -Path $logfile -ItemType File
+
+#Backup starting on logfile
+Write-Output "Backup Starting `n`n" | Out-File $logfile -Append
+
+#Begin Backup
+wbAdmin start backup -backupTarget:$backup_destination -include:$backup_source -allCritical | Out-File $logfile -Append
+
+Write-Output "Backup Success" | Out-File $logfile -Append
+
+Write-Output "Spawning External Backup Process" | Out-File $logfile -Append
+& "$PSScriptRoot\copy_backup_ext.ps1"
